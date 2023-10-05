84c84bb030775e87576a95efc6af363981cc5a19
hasegii <zxc.perm@mail>
2023-10-05 11:33:58 +0500

    init

---
+++ bin/kontr2.dart
@@ -0,0 +1,5 @@
+import 'package:kontr2/kontr2.dart' as kontr2;
+
+void main(List<String> arguments) {
+  print('Hello world: ${kontr2.calculate()}!');
+}
---
+++ test/kontr2_test.dart
@@ -0,0 +1,8 @@
+import 'package:kontr2/kontr2.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
